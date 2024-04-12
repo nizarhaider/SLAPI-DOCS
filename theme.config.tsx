2c70339e4732368bb4e5430d6b932dfecb7fc4d2
Nizar <43636043+nizarhaider@users.noreply.github.com>
2024-04-12 16:58:12 +0530

    updated title

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,6 +11,13 @@ const config: DocsThemeConfig = {
       </span>
   </>  
   ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="SLAPI" />
+      <meta property="og:description" content="Sri Lankan data indicators" />
+    </>
+  ),
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
